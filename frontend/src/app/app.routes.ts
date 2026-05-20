cc51c98861f17a29bebd4754867aed7ad02b90df
ChristinaHassan <christinahasd@gmail.com>
2026-05-20 12:31:20 +0300

    Fixes #2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
+import { SignInComponent } from './components/signin/signin';
+import { JobsComponent } from './components/jobs/jobs';
+import { SkillsComponent } from './components/skills/skills';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', redirectTo: '/login', pathMatch: 'full' },
+{ path: 'login', component: LoginComponent },
+{ path: 'signin', component: SignInComponent },
+{ path: 'skills', component: SkillsComponent },
+{ path: 'jobs', component: JobsComponent }
+
+];
